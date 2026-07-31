b26138a5028d11d08d51aa574b41939d54605668
Guilherme <guilhermefemix2010@gmail,com>
2026-07-30 23:07:51 -0300

    Primeiro codigo dentro de uma função, gostei bastante de usar funções, logo logo eu volto nos meus programas e adiciono as funções neles

---
+++ Fundamentos/15_Funções_Continuação.py
@@ -0,0 +1,10 @@
+def multiplicar():
+    numero_1= int(input("Digite um número: "))
+    numero_2= int(input("Digite outro número: "))
+    multiplique= numero_1 * numero_2
+    print(f"O resultado da sua operação é: {multiplique}") 
+
+multiplicar()
+
+multiplicar()
+
