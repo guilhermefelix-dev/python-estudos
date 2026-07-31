8492094eb73a36ae5404787d560d92ba4f50881c
Guilherme <guilhermefemix2010@gmail,com>
2026-07-30 22:35:58 -0300

    Aqui eu fiz o uso de uma função pela primeira vez

---
+++ Fundamentos/14_Funções.py
@@ -0,0 +1,10 @@
+def linhas():
+    print("=======================")
+
+linhas()
+print("BIBLIOTECA GALÁCTICA")
+linhas()
+print("1- Emprestar Livro")
+print("2- Devolver Livro")
+print("3- Livros Emprestados")
+linhas()
